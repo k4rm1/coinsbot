3ec17076d0d3c9ca45f98782563439acf2a621b5
! Ruwin.zkr <110337782+Ruwin-dsc@users.noreply.github.com>
2024-04-16 13:22:04 +0200

    Update managerCoins.sql /uhq

--- dbsql/managerCoins.sql
+++ dbsql/managerCoins.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 16 avr. 2024 à 11:03
+-- Généré le : mar. 16 avr. 2024 à 13:21
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.2.0
 
@@ -20,6 +20,42 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `managerCoins`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Bots`
+--
+
+CREATE TABLE `Bots` (
+  `id` varchar(255) NOT NULL,
+  `botid` varchar(65) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `default_prefix` varchar(255) DEFAULT NULL,
+  `owner` varchar(255) DEFAULT NULL,
+  `max_guild` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `DateStart` date DEFAULT NULL,
+  `Duration` varchar(255) DEFAULT NULL,
+  `Status` varchar(255) DEFAULT NULL,
+  `activity` varchar(255) DEFAULT NULL,
+  `activitytype` varchar(255) DEFAULT NULL,
+  `lastWarningTime` date DEFAULT NULL,
+  `Whitelist` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}' CHECK (json_valid(`Whitelist`)),
+  `hasWarned` tinyint(1) DEFAULT NULL,
+  `createdAt` varchar(255) DEFAULT NULL,
+  `Owners` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '{}' CHECK (json_valid(`Owners`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Bots`
+--
+ALTER TABLE `Bots`
+  ADD PRIMARY KEY (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
